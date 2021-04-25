1e3f68e5b0ebc10b8daff832ff8779daf304c6c1
Oscar Omar Pineda <oscarpineda05@gmail.com>
2021-04-25 15:55:14 -0600

    Se agregó la clase CLCiudad

---
+++ src/main/java/com/curlp/capalogica/CLCiudad.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.curlp.capalogica;
+
+/**
+ *
+ * @author oscarpineda
+ */
+public class CLCiudad {
+    private int ciudadID;
+    private String nombreCiudad;
+
+    public int getCiudadID() {
+        return ciudadID;
+    }
+
+    public void setCiudadID(int ciudadID) {
+        this.ciudadID = ciudadID;
+    }
+
+    public String getNombreCiudad() {
+        return nombreCiudad;
+    }
+
+    public void setNombreCiudad(String nombreCiudad) {
+        this.nombreCiudad = nombreCiudad;
+    }
+    
+    
+}
