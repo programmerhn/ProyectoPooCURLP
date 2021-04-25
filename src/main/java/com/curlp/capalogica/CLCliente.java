2c0359cf00c68ba5080ea482a5042a11c0c4a252
AngelZambrano98 <joseangel.mendoza98@hotmail.com>
2021-04-25 15:59:52 -0600

    Se agregó la clase CLCliente en la Capa Lógica

---
+++ src/main/java/com/curlp/capalogica/CLCliente.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.curlp.capalogica;
+
+/**
+ *
+ * @author josea
+ */
+public class CLCliente {
+    private int clienteID;
+    private String nombres;
+    private String apellidos;
+
+    public int getClienteID() {
+        return clienteID;
+    }
+
+    public void setClienteID(int clienteID) {
+        this.clienteID = clienteID;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    
+}
